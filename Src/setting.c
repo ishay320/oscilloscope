9fb6a7008e8709f66623cd2b3b63f335e1c54b22
ishay320 <ishay320@hotmail.com>
2022-11-12 22:54:17 +0200

    removed if def cpp

--- Src/setting.c
+++ Src/setting.c
@@ -1,57 +1,49 @@
-#ifdef __cplusplus
-extern "C"
-{
-#endif
 #include "setting.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-    void configSetting(const char* command, Setting* setting)
+void configSetting(const char* command, Setting* setting)
+{
+    const size_t command_size = strlen(command);
+    int char_parsed           = 0;
+
+    switch (command[char_parsed++])
     {
-        const size_t command_size = strlen(command);
-        int char_parsed           = 0;
+        case 't':  // trigger
+            switch (command[char_parsed++])
+            {
+                case 'u':  // up
+                    setting->trigger_direction = UP;
+                    break;
+                case 'd':  // down
+                    setting->trigger_direction = DOWN;
+                    break;
+                case 's':  // stop
+                    setting->trigger_direction = STOP;
+                    break;
 
-        switch (command[char_parsed++])
-        {
-            case 't':  // trigger
-                switch (command[char_parsed++])
-                {
-                    case 'u':  // up
-                        setting->trigger_direction = UP;
-                        break;
-                    case 'd':  // down
-                        setting->trigger_direction = DOWN;
-                        break;
-                    case 's':  // stop
-                        setting->trigger_direction = STOP;
-                        break;
+                default:
+                    printf("ERROR: command '%s'is not recognized in char %d\r\n", command, char_parsed);
+                    break;
+            }
+            if (command_size - char_parsed > 0)
+            {
+                setting->trigger_volt = atoi(command + char_parsed);
+            }
+            return;
 
-                    default:
-                        printf("ERROR: command '%s'is not recognized in char %d\r\n", command, char_parsed);
-                        break;
-                }
-                if (command_size - char_parsed > 0)
-                {
-                    setting->trigger_volt = atoi(command + char_parsed);
-                }
-                return;
+            break;
+        case 's':  // adc speed
+            printf("ERROR: command '%s' is not setup yet\r\n", command);
+            break;
+        case 'o':  // output
+            printf("ERROR: command '%s' is not setup yet\r\n", command);
+            break;
 
-                break;
-            case 's':  // adc speed
-                printf("ERROR: command '%s' is not setup yet\r\n", command);
-                break;
-            case 'o':  // output
-                printf("ERROR: command '%s' is not setup yet\r\n", command);
-                break;
-
-            default:
-                printf("ERROR: command '%s' is not setup yet\r\n", command);
-                break;
-        }
+        default:
+            printf("ERROR: command '%s' is not setup yet\r\n", command);
+            break;
     }
-
-#ifdef __cplusplus
 }
-#endif
