67add2142df4a828771485cce574889c021a6685
ishay320 <ishay320@hotmail.com>
2022-11-17 08:35:25 +0200

    added parsing for rewind and for volt

--- Src/setting.c
+++ Src/setting.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+static void parseTriggerSetting(const char* command, Setting* setting, int* char_parsed, const size_t command_size);
+
 void configSetting(const char* command, Setting* setting)
 {
     const size_t command_size = strlen(command);
@@ -16,9 +18,11 @@ void configSetting(const char* command, Setting* setting)
             {
                 case 'u':  // up
                     setting->trigger_direction = UP;
+                    parseTriggerSetting(command, setting, &char_parsed, command_size);
                     break;
                 case 'd':  // down
                     setting->trigger_direction = DOWN;
+                    parseTriggerSetting(command, setting, &char_parsed, command_size);
                     break;
                 case 's':  // stop
                     setting->trigger_direction = STOP;
@@ -27,10 +31,6 @@ void configSetting(const char* command, Setting* setting)
                 default:
                     printf("ERROR: command '%s'is not recognized in char %d\r\n", command, char_parsed);
                     break;
-            }
-            if (command_size - char_parsed > 0)
-            {
-                setting->trigger_volt = atoi(command + char_parsed);
             }
             return;
 
@@ -45,5 +45,27 @@ void configSetting(const char* command, Setting* setting)
         default:
             printf("ERROR: command '%s' is not setup yet\r\n", command);
             break;
+    }
+}
+
+static void parseTriggerSetting(const char* command, Setting* setting, int* char_parsed, const size_t command_size)
+{
+    while (command_size > *char_parsed)
+    {
+        switch (command[(*char_parsed)++])
+        {
+            case 'v':  // volt
+                if (command_size - *char_parsed > 0)
+                {
+                    setting->trigger_volt = atoi(command + *char_parsed);
+                }
+                break;
+            case 'r':  // rewind
+                if (command_size - *char_parsed > 0)
+                {
+                    setting->rewind = atoi(command + *char_parsed);
+                }
+                break;
+        }
     }
 }
